a3a36196ef8f382edbe989f95a14ad3e3c42111a
duongnh <duonghainguyen000@gmail.com>
2025-06-22 12:18:12 +0700

    update

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import streamlit as st
-# Thay đổi: Import ChatGoogleGenerativeAI thay vì ChatGroq
 import google.generativeai as genai
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
@@ -13,7 +12,7 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
-from langchain_core.prompts import ChatPromptTemplate # Thêm import này cho Gemini
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
